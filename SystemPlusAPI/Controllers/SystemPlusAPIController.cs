ef3078d29044102c271fcec3c2e9cbddd2acc1d4
Vanja Kondac <v.kondac@vegait.rs>
2022-11-21 22:33:10 +0100

    API can now list all vehicles, get single vehicle by id, and create a vehicle.

---
+++ SystemPlusAPI/Controllers/SystemPlusAPIController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SystemPlusAPI.Data;
+using SystemPlusAPI.Models;
+using SystemPlusAPI.Models.Dto;
+
+namespace SystemPlusAPI.Controllers
+{
+    [Route("api/SystemPlusAPI")]
+    [ApiController]
+    public class SystemPlusAPIController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VehicleDTO>> GetVehicles()
+        {
+            return Ok(VehicleStore.vehicles);
+        }
+        [HttpGet("id:int")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VehicleDTO> GetSingleVehicle(int id)
+        {   
+            if(id== 0)
+            {
+                return BadRequest();
+            }
+            var vehicle = VehicleStore.vehicles.FirstOrDefault(x => x.Id == id);
+            if(vehicle == null)
+            {
+                return NotFound();
+            }
+            return Ok(vehicle);
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VehicleDTO> CreateVehicle([FromBody]VehicleDTO vehicle)
+        {
+            if (vehicle == null)
+            {
+                return BadRequest(vehicle);
+            }
+            if(vehicle.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            vehicle.Id = VehicleStore.vehicles.Max(x => x.Id)+1;
+            VehicleStore.vehicles.Add(vehicle);
+            return Ok(vehicle);
+        }
+    }
+}
+
+
---
+++ SystemPlusAPI/Models/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace SystemPlusAPI.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int Kw { get; set; }
+        public int Cm { get; set; }
+        public int Year { get; set; }
+        public int PremiumNumber { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
