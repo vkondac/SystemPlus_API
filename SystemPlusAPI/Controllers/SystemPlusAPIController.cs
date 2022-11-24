47a4be11906d3b51a7a5e0a3218895a28372ccb8
Vanja Kondac <v.kondac@vegait.rs>
2022-11-24 14:07:53 +0100

    Added PUT and DELETE.

--- SystemPlusAPI/Controllers/SystemPlusAPIController.cs
+++ SystemPlusAPI/Controllers/SystemPlusAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using SystemPlusAPI.Data;
 using SystemPlusAPI.Models;
 using SystemPlusAPI.Models.Dto;
@@ -47,6 +48,42 @@ namespace SystemPlusAPI.Controllers
             }
             vehicle.Id = VehicleStore.vehicles.Max(x => x.Id)+1;
             VehicleStore.vehicles.Add(vehicle);
+            return Ok(vehicle);
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VehicleDTO> DeleteVehicle(int id)
+        {
+            var vehicle = VehicleStore.vehicles.FirstOrDefault(x => x.Id == id);
+            if (vehicle == null)
+            {
+                return NotFound("Vehicle with that id does not exist");
+            }
+            if(id < 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            VehicleStore.vehicles.Remove(vehicle);
+            return Ok(vehicle);
+        }
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VehicleDTO> EditVehicle(int id, [FromBody] VehicleDTO vehicleDTO)
+        {
+            var vehicle = VehicleStore.vehicles.FirstOrDefault(x => x.Id == id);
+            if (vehicleDTO == null)
+            {
+                return NotFound("Vehicle with that id does not exist");
+            }
+            vehicle.PremiumNumber = vehicleDTO.PremiumNumber;
+            vehicle.Year= vehicleDTO.Year;
+            vehicle.Cm = vehicleDTO.Cm;
+            vehicle.Kw= vehicleDTO.Kw;
             return Ok(vehicle);
         }
     }
